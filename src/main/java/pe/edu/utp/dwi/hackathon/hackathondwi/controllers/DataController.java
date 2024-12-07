ee3fbc00de5a52d24e65f1aa1707d3d04a08bf12
Luis Bazán <luis@bazan.pe>
2024-12-07 09:46:59 -0500

    Update sidebar links

--- src/main/java/pe/edu/utp/dwi/hackathon/hackathondwi/controllers/DataController.java
+++ src/main/java/pe/edu/utp/dwi/hackathon/hackathondwi/controllers/DataController.java
@@ -12,7 +12,6 @@ public class DataController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String pathInfo = req.getPathInfo();
-        System.out.println(pathInfo);
         if (pathInfo != null && pathInfo.length() > 1) {
             String sectionId = pathInfo.substring(1);
             req.setAttribute("sectionId", sectionId);
--- src/main/java/pe/edu/utp/dwi/hackathon/hackathondwi/controllers/StatsController.java
+++ src/main/java/pe/edu/utp/dwi/hackathon/hackathondwi/controllers/StatsController.java
@@ -1,4 +1,5 @@
 package pe.edu.utp.dwi.hackathon.hackathondwi.controllers;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,10 +8,20 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(value="/stats")
+@WebServlet(value = "/stats/*")
 public class StatsController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("stats.xhtml").forward(req, resp);
+        String pathInfo = req.getPathInfo();
+        if (pathInfo != null && pathInfo.length() > 1) {
+            String sectionId = pathInfo.substring(1);
+            req.setAttribute("sectionId", sectionId);
+        } else {
+            // Redirect
+            resp.sendRedirect("/home");
+            return;
+        }
+
+        req.getRequestDispatcher("/stats.xhtml").forward(req, resp);
     }
 }
